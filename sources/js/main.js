ae13935bc4df0e205526be2ccba88726bb354528
MarlonMorales232409 <marlon.morales2324@icloud.com>
2021-08-02 20:21:44 -0400

    Ubicacion del usuario desde JavaScript

---
+++ sources/js/main.js
@@ -0,0 +1,24 @@
+"use strict";
+
+// Geolocalizacion por el fronted
+
+// const position = navigator.geolocation;
+
+// const getPosition = (pos)=> {
+//     let coordenadas = `${pos.coords.latitude},${pos.coords.longitude}`;
+//     fetch(`api/?coordenadas=${coordenadas}`)
+//     .then(respuesta => respuesta.json())
+//     .then(data => {
+//         console.log( data );
+//     });
+// }
+// const getErrorPosition = ()=> {
+//     return "Error ocurrido"
+// }
+
+// position.getCurrentPosition(getPosition,getErrorPosition);
+
+// Obtener datos por el codigo postal de la ciudad 
+
+
+
