ea461cf8854a3de259dc419b58f5dc99a3f95466
MarlonMorales232409 <marlon.morales2324@icloud.com>
2021-08-11 15:48:07 -0400

    Optimizado el main.js para modificar el DOM

--- sources/js/main.js
+++ sources/js/main.js
@@ -1,109 +1,36 @@
-// const tempStructure = (data)=> {
+import { Weather } from "./weather_request.js";
+import { DateInfo } from "./date_info.js";
 
-//     // Creando los elementos de la zona de temperatura
-
-//     const divMainTemp = document.createElement('article');
-//     const h2Temp = document.createElement('h2')
-//     const divTempSecundary = document.createElement('article');
-//     const tempMin =document.createElement('p');
-//     const tempFeel =document.createElement('p');
-//     const tempMax =document.createElement('p');
-
-//     // Agregando las clases
-
-//     divMainTemp.classList.add("main__temp-container");
-//     h2Temp.classList.add("main__temp");
-//     divTempSecundary.classList.add("main__temp-secundary");
-//     tempMin.classList.add("main__temp-min");
-//     tempFeel.classList.add("main__temp-feel-like");
-//     tempMax.classList.add("main__temp-max");
-
-//     // Agregando datos a los elementos
-
-//     h2Temp.textContent = `Temperatura Actual ${data.main.temp}°`;
-//     tempMin.textContent = `Temperatura minima ${data.main.temp_min}°`;
-//     tempFeel.textContent = `Se siente como unos ${data.main.feels_like}°`;
-//     tempMax.textContent = `Temperatura maxima ${data.main.temp_max}°`;
-
-//     // Organizando los contenedores
-
-//     divTempSecundary.appendChild(tempMin);
-//     divTempSecundary.appendChild(tempFeel);
-//     divTempSecundary.appendChild(tempMax);
-//     divMainTemp.appendChild(h2Temp);
-//     divMainTemp.appendChild(divTempSecundary);
-
-//     return divMainTemp;
-
-// }
-
-// const weatherStructure = (data)=> {
-
-//     // Creando los elementos de la zona de temperatura
-
-//     const mainArticle = document.createElement('article');
-//     const imgContainer = document.createElement('article')
-//     const img = document.createElement('img');
-//     const DescriptionCont =document.createElement('article');
-//     const generalWeather =document.createElement('h2');
-//     const description =document.createElement('h2');
-
-//     // Agregando las clases
-
-//     mainArticle.classList.add("main__wheather-container");
-//     imgContainer.classList.add("main_wheather-img-container");
-//     img.classList.add("main__wheather-img");
-//     DescriptionCont.classList.add("main__wheather-description-container");
-//     generalWeather.classList.add("main__wheather-title");
-//     description.classList.add("main__wheather-main");
-
-//     // Agregando datos a los elementos
-
-//     img.src = data.weather[0].iconos.iconD;
-//     generalWeather.textContent = data.weather[0].main;
-//     description.textContent = data.weather[0].description;
-
-//     // Organizando los contenedores
-
-//     mainArticle.appendChild(imgContainer);
-//     imgContainer.appendChild(img);
-//     DescriptionCont.appendChild(generalWeather);
-//     DescriptionCont.appendChild(description);
-//     mainArticle.appendChild(DescriptionCont);
-    
-//     return mainArticle;
-// } 
-
-const getData = async ()=> {
-    const request = await fetch("api/");
-    const data = await request.json();
-    if(data.info == "La URL introducida no es válida") return false;
-    else return data;
+const updateDateInfo = (data)=> { 
+    // Funcion que modifica los datos de la fecha instanciando a la clase DateInfo()
+    document.querySelector(".header__day").textContent = data[0];
+    document.querySelector(".header__date-day").textContent = data[1];
+    document.querySelector(".header__date-month").textContent = data[2];
+    document.querySelector(".header__date-year").textContent = data[3];
 }
 
-let clima = await getData();
-
-if(clima == false) {
-    alert('No se pudo detectar tu ubicacion')
-}else {
-    
-    // let block = document.querySelector('.main__container');
-
-    // let dataTemp = tempStructure(clima);
-    // const fragmentTemp = document.createDocumentFragment();
-    // fragmentTemp.appendChild(dataTemp);
-    // block.appendChild(fragmentTemp);
-
-    // console.log(clima);
-
-    // let dataWeather = weatherStructure(clima);
-    // const fragmentWeather = document.createDocumentFragment();
-    // fragmentWeather.appendChild(dataWeather);
-    // block.appendChild(fragmentWeather);
-    
-    // Empezar a Trabajar desde aqui
-    
+const updateWeatherInfo =  async ()=> {
+    // Funcion que modifica los datos del clima de manera asincrona instanciando a la clase Weather()
+    let weatherInfo = await new Weather("api/").getWeather();
+    if(weatherInfo == false) {
+        alert('No se pudo detectar tu ubicacion, verifica tu conexion a internet y recarga la pagina');
+        clearInterval(interval);
+    }else {
+        document.querySelector(".main__temp").textContent = `${weatherInfo.main.temp}`;
+        document.querySelector(".main__weather").textContent = `${weatherInfo.weather[0].description}`;
+        document.querySelector(".main__humidity").textContent = `${weatherInfo.main.humidity}`;
+        document.querySelector(".main__wind").textContent = `${weatherInfo.wind.speed}`;
+        document.querySelector(".main__city-name").textContent = `${weatherInfo.city}`;
+    }
 }
+
+updateDateInfo(new DateInfo().getActualDate());
+
+updateWeatherInfo(); // Se llama a la funcion para que muestre por primera ves los datos del clima
+// Se establece un intervalo para que el clima se actualice cada 30 minutos
+const interval = setInterval(updateWeatherInfo, 1800000);
+
+
 
 
 
